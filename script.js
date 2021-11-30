dbb05679e8985f5b8b4225a410b82cbfccfa7007
dmknapp2385 <dmknapp2385@gmail.com>
2021-11-30 07:38:18 -0700

    row styling

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 // create today's date and time variable
-var today = moment(); 
-console.log(today);
+var today = moment();
 
-$("#currentDay").text(today.format('MMM Do YYYY'));
+// add date to top of page
+$("#currentDay").text(today.format('dddd MMMM Do'));
+
+// set timer interval to audit planner every ten minutes to see if hour has passed
+setInterval (function () {
+    auditHours(); }, 10000
+);
+
+//create task function
+
+$('#hours').on("click", "li", function () {
+    
+})
+
+// save task function to store local
+
+//audit function to see if hour has passed and assigns color
+
+
+// new day reload function to clear all tasks at end of the day
