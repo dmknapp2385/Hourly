b40f06b96aca918eeda9fe14855974b67044f3b0
dmknapp2385 <dmknapp2385@gmail.com>
2021-12-01 09:55:18 -0700

    if else and .each loop fixed

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// create today's date and time variable
-var today = moment();
 var tasksArray = [];
-var currentHour = parseInt(moment().format("H"));
 
 // add date to top of page
-$("#currentDay").text(today.format('dddd MMMM Do'));
+$("#currentDay").text(moment().format('dddd MMMM Do'));
 
 // set timer interval to audit planner every ten minutes to see if hour has passed
-// setInterval (function () {
-//     auditHours(); }, 10000
-// );
+setInterval (function () {
+    auditHours(); }, 10000
+);
 
 //create task function
 $('.row').on("click", ".saveBtn", function () {
@@ -33,26 +30,27 @@ var auditHours = function () {
         if (hour <= 5 && hour >= 1) {
             hour += 12;
         }
-        console.log(hour);
-    
-        // switch to compare to current time
-        switch (hour) {
-            case hour < currentHour:
-                $(this).addClass("past");
-                break;
-            case hour === currentHour:
-                $(this).addClass("present");
-                break;
-            default:
-                $(this).addClass("future");
-                break;
-            }
-        
+        // var currentHour = parseInt(moment().format("H"));  
+        var currentHour = 18;  
+        // if/else to compare to current time
+        if (hour < currentHour) {
+            $(this).addClass("past");
+            console.log(hour < currentHour);
+        }
+        else if (hour === currentHour) {
+            $(this).addClass("present");
+        }
+
+        else if (hour > currentHour && hour < 18) {
+            $(this).addClass("future");
+        }
+
+        else if (hour === 18) {
+            return removeTasks();
+        }
     }) 
 };
     
-
-    // if beyond 5, new day reload  
 
 // load task function takes local storage and displays
 var loadTasks = function () {
@@ -64,11 +62,18 @@ var loadTasks = function () {
         var text = task.task;
         // assign to text area with corresponding id
         $("#" + id).val(text);
-        // audit hours
-        auditHours();
     })
+    auditHours();
 }
 
 loadTasks();
+
+// if beyond 5, new day reload  
+var removeTasks = function() {
+    $("textarea").each(function() {
+        $(this).val("");
+    })
+    localStorage.clear();
+}
 
 // new day reload function to clear all tasks at end of the day
